172789d05416845175ef2af20e1cd9f6f1d6de45
unknown <shaulayham@yahoo.com>
2022-08-30 21:50:29 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import image1 from './dsc2.jpg'
 import './App.css';
+import React from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+
+            <div className="image-header">
+                <div className="nav">
+                    <div className="left">
+                        <a href="">AR SHAKIR</a>
+                    </div>
+                    <div className="center">
+                        <ul>
+                            <li>
+                                <a href="">Template</a>
+                            </li>
+                            <li>
+                                <a href="">Product</a>
+                            </li>
+                            <li>
+                                <a href="">Pricing</a>
+                            </li>
+                            <li>
+                                <a href="">Pricing</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="right">
+                        <button className="transparent">Sign-up</button>
+                        <button>Start-free</button>
+                    </div>
+
+
+                </div>
+                <img src={image1} alt=""/>
+            </div>
+        </div>
+    );
 }
 
 export default App;
